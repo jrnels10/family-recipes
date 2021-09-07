29ac5516fcbfc902784c3e4ea98ec05b54dae0b1
Jacob Nelson <jrnels10@gmail.com>
2021-09-06 18:10:34 -0700

    removed toekin

--- src/Data/API/recipe.service.tsx
+++ src/Data/API/recipe.service.tsx
@@ -5,13 +5,6 @@ import AuthService from "./auth.service";
 import BaseHttpService from "./base-http.service";
 
 export default class RecipeService extends BaseHttpService {
-    //     async getWeather() {
-    //         // const res = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=-112.2&lon=33.3&exclude=minutely,daily&appid=380b494a0c7f5b2c737c6f0e8bb71c07`);
-    //         // console.log(JSON.stringify(res.data))
-    //         const res= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=33.3&lon=-112&exclude=minutely,daily&appid=380b494a0c7f5b2c737c6f0e8bb71c07`);
-    //         debugger
-    // return res;
-    //     };getalluser
     async uploadImage(id: number, blobData: string, name: string, ext: string) {
         const formData = new FormData();
         formData.append('file', blobData, `myimage.${ext}`);
